b6806062aeb89f68388c56906c49e1f5e0659774
Igor Kryvenko <ikryvenk@e1r8p16.unit.ua>
2017-05-26 16:39:31 +0300

    merge ik and ap

--- corewar-championship/test.s
+++ corewar-championship/test.s
@@ -1,7 +1,5 @@
 .name		"S.A.M."
 .comment	"Ca roxxe du chameau"
 
-label:
-	-1, r1
 ld -1, r1
 st r1, 10
---
+++ virtual_machine/ik_init_field.c
@@ -0,0 +1,9 @@
+#include "virtual_machine.h"
+
+void    start_vm(t_struct *pl)
+{
+    int i;
+
+    i = pl->num_pl;
+    while (i > 0)
+}
